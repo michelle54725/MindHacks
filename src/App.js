1e6d050fbc419276391395bc505f71b29aa0c274
michelle54725 <michelle54725@gmail.com>
2017-10-07 10:04:30 -0700

    Learning React by experimentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+//components = anything that can be passed into <>
+
+function MindHacks(props) {
+//props = properties = stores data its representing, which is data that will be provided to Component to change)
+  return <span> Author: {props.author}. Version: {props.version}.0. MindHacks!!! </span>;
+//in later <MindHacks/> call: opening tag with slash at end = both opening/closing tags
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        A component!
+        <em> A component!! </em>
+        <MindHacks author='Oski' version={4}/>
       </div>
     );
   }
