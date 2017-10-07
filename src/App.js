6c10e75c90e46f88deeca53dc808ead7daaf39e3
michelle54725 <michelle54725@gmail.com>
2017-10-07 11:59:52 -0700

    Add Nameform

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 //components = anything that can be passed into <>
 
-function MindHacks(props) {
+function Title(props) {
 //props = properties = stores data its representing, which is data that will be provided to Component to change)
-  return <span> Author: {props.author}. Version: {props.version}.0.</span>;
+  return (
+    <div id="Title">
+    <h1> Welcome {props.name}! Damn, look at your MindHack: </h1>
+      <h2> Author: {props.author}. Version: {props.version}.0.</h2>;
+    </div>
+  )
+}
+  //in later <MindHacks/> call: opening tag with slash at end = both opening/closing tags
+
+function Paragraph(props) {
+//props = properties = stores data its representing, which is data that will be provided to Component to change)
+  return <p> This is paragraph #{props.paragraph}</p>;
 //in later <MindHacks/> call: opening tag with slash at end = both opening/closing tags
 }
 
@@ -12,11 +23,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <em> A component!! </em>
-        <MindHacks author='Oski' version={4}/>
-      </div>
-    );
+        <Title name='Name' author='Oski' version={4}/><br />
+        <Paragraph paragraph={1}/>
+        </div>
+       );
   }
 }
 
-export default App;
+export default App
