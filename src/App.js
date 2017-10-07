4365c3da5d4ed740804ef5fb2b67da39c56113ce
michelle54725 <michelle54725@gmail.com>
2017-10-07 14:40:27 -0700

    Implement Path Abstraction

--- src/App.js
+++ src/App.js
@@ -11,12 +11,36 @@ function MindHacks(props) {
 // Developed by:
 // William Brandon, Grace Lin, Myles Domingo, Michelle Mao
 
+//paths
+const empty = []
+function addToPath(path, point) {
+//point: [x,y]
+  const new_path = path.slice()
+  new_path.push(point)
+  return new_path
+}
+
+//view for path
+function DisplayPath(props) {
+    //svg expects strings, so convert to strings
+  const pointStrings = props.path.map((each)=>each[0].toString() + ',' + each[1].toString())
+  const pointSingleString = pointStrings.join(' ')
+  return <polyline fill="none" stroke="black"
+  points={pointSingleString}/>
+}
+
+let path = addToPath(empty, [100,100])
+path = addToPath(path, [200,100])
+
 class App extends Component {
   render() {
     return (
       <div>
         <em> A component!! </em>
         <MindHacks author='Oski' version={4}/>
+        <svg>
+        <DisplayPath path={path}/>
+        </svg>
       </div>
     );
   }
